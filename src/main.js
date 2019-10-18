ba47e135335f1398db991f0cf5064703174fc77b
Nik Pietanze <nik@Niks-MacBook-Pro.local>
2019-10-18 16:28:35 -0400

    v1.0.0

--- src/main.js
+++ src/main.js
@@ -10,13 +10,7 @@ Vue.use(VueTextAreaAutosize);
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA0bSAql-17YE7jNG6j3d7nJ498FDYorKA",
-  authDomain: "vue-calendar-852c6.firebaseapp.com",
-  databaseURL: "https://vue-calendar-852c6.firebaseio.com",
-  projectId: "vue-calendar-852c6",
-  storageBucket: "vue-calendar-852c6.appspot.com",
-  messagingSenderId: "61597968067",
-  appId: "1:61597968067:web:35b98d29e5d58cd46d6476"
+  // Firebase Info
 })
 
 export const db = firebase.firestore();
